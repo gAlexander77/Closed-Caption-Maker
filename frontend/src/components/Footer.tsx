1111f5a919cdd84d91b310d9cd3bcdd1acc4d05c
galexander77 <87527303+gAlexander77@users.noreply.github.com>
2024-01-01 22:29:04 -0600

    Update

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,11 +1,29 @@
 import React from "react";
+import { BsGithub } from "react-icons/bs";
 import "../styles/components/Footer.css";
 
 const Footer = () => {
+    const currentYear: number = new Date().getFullYear();
+    const sourceCodeLink: string =
+        "https://github.com/gAlexander77/Closed-Caption-Video-Maker";
+
     return (
         <div className="footer-component">
-            <div>Closed Caption Maker</div>
-            <div></div>
+            <div className="footer-content">
+                <a
+                    className="source-code-link"
+                    href={sourceCodeLink}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <BsGithub />
+                    Source Code
+                </a>
+                <p className="copyright">
+                    ©{currentYear} Closed Caption Maker | Made by Alexander
+                    Martinez
+                </p>
+            </div>
         </div>
     );
 };
--- frontend/src/pages/Home/components/Welcome.tsx
+++ frontend/src/pages/Home/components/Welcome.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from "react";
 import "../../../styles/pages/Home/components/Welcome.css";
-import backgroundVideo from "../../../assets/welcome.mp4";
+import background from "../../../assets/background.jpg";
 
 interface WelcomeProps {
     setRoute: (route: string) => void;
@@ -12,19 +12,17 @@ const Welcome: FC<WelcomeProps> = ({ setRoute }) => {
     };
 
     return (
-        <div className="welcome-container">
-            {/* <video autoPlay loop muted className="background-video">
-                <source
-                    src={backgroundVideo}
-                    type="video/mp4"
-                    style={{ all: "unset" }}
-                />
-                Your browser does not support the video tag.
-            </video> */}
-            <h1>Welcome</h1>
-            <h1>Translate and add closed captions here</h1>
-            <button onClick={handleClick}>Get Started</button>
-        </div>
+        <>
+            <div className="welcome-background-container">
+                <div className="background-filter"></div>
+                <img className="background-image" src={background}></img>
+            </div>
+            <div className="welcome-container">
+                <h1>Welcome</h1>
+                <h1>Translate and add closed captions here</h1>
+                <button onClick={handleClick}>Get Started</button>
+            </div>
+        </>
     );
 };
 
